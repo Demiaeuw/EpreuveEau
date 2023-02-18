d942df2d01d104545f57726364509f0f7f526915
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-18 21:17:08 +0100

    avance exercice 03

--- eau03.py.py
+++ eau03.py.py
@@ -1,6 +1,14 @@
 #Suite de Fibonacci
 #Créez un programme qui affiche le N-ème élément de la célèbre suite de Fibonacci. (0, 1, 1, 2) étant le début de la suite et le premier élément étant à l’index 0.
 
+
+
+#Definir la suite de Fibonacci
+#l'intégré dans une liste
+#gerer la demande d'argument en lien avec la liste
+
+
+
 result = 0
 listeFibonacci = []
 
@@ -10,5 +18,4 @@ def Fibonacci():
     else:
         result = result + result 
     print(result)
-
 
