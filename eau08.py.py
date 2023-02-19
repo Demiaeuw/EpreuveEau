b6cba8abd8467bb1ee9465d637eb00423980d307
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-19 18:24:59 +0100

    exo 8 ok

--- eau08.py.py
+++ eau08.py.py
@@ -1,2 +1,16 @@
 #Chiffres only
 #Créez un programme qui détermine si une chaîne de caractères ne contient que des chiffres.
+
+
+import sys
+
+argument = sys.argv[1]
+
+try:
+    argument = int(sys.argv[1])
+    if len(sys.argv) != 2:
+        print("error")
+    else:
+        print("true")
+except ValueError:
+    print("error")
