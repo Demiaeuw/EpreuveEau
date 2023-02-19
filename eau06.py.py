dc7fc521d58c26cf12351396a2a4d5583499bfab
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-19 18:29:51 +0100

    debut exerci 06

--- eau06.py.py
+++ eau06.py.py
@@ -1,2 +1,24 @@
 #Majuscule sur deux
 #Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères. Seule les lettres (A-Z, a-z) sont prises en compte.
+
+import sys
+
+argument = sys.argv[1]
+
+
+
+
+
+
+
+
+
+
+try:
+    argument = str(sys.argv[1])
+    if len(argument) != 2:
+        print("error")
+    else:
+        print(result)
+except ValueError:
+    print("error")
