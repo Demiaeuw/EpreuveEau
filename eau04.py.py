b3e929dd6b6c39844977f5c83dbd1e42d1091d6a
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-19 16:59:26 +0100

    probleme de stop +1

--- eau04.py.py
+++ eau04.py.py
@@ -1,10 +1,17 @@
 #Prochain nombre premier
 #Créez un programme qui affiche le premier nombre premier supérieur au nombre donné en argument.
 
+#verif argument nombre premier
+#créer une liste de nombre premier 
+#arreté la liste a la demande
+#afficher le suivant 
 
 import sys
 
-def nombre_premier(n):      #test nombre premier
+stop = sys.argv[1]
+
+
+def nombre_premier(n):          #verif argument nombre premier
     if n <= 1:
         return False
     for i in range(2, n):
@@ -12,3 +19,18 @@ def nombre_premier(n):      #test nombre premier
             return False
     return True
 
+def liste_premiers(n):          #créer une liste de nombre premier 
+    primes = []
+    for i in range(2, n):
+        if nombre_premier(i):
+            primes.append(i)
+    return primes
+
+
+try:
+    stop = int(sys.argv[1])     #arreté la liste a la demande
+    demand = stop + 1           #!!!!!!!!!! afficher le suivant !!!!!!!!!!!!
+    result = liste_premiers(demand)
+    print(result[-1])
+except ValueError:
+    print("-1")
