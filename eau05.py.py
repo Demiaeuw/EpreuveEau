286f3b8d16bd09fc7592eb4c7bbdd413ace2a17d
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-20 14:35:10 +0100

    fin exercice 05

--- eau05.py.py
+++ eau05.py.py
@@ -1,2 +1,22 @@
 #String dans string
 #Créez un programme qui détermine si une chaîne de caractère se trouve dans une autre.
+
+import sys
+
+val1 = sys.argv[1]
+val2 = sys.argv[2]
+argumentstest = sys.argv[1:]
+
+try:
+    val1 = str(sys.argv[1])
+    val2 = str(sys.argv[2])
+    if len(argumentstest) != 2:
+        print("entrer 2 arguments")
+    elif val2 in val1 or val1 in val2:
+        print("true")
+    else:
+        print("false")
+
+
+except ValueError:
+    print("Error")
