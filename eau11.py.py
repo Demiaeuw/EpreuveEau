0e202c82924abba26b2f73bd9d8d813c17a712e2
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-28 14:35:32 +0100

    fin exo 11

--- eau11.py.py
+++ eau11.py.py
@@ -1,2 +1,35 @@
 #Différence minimum absolue
 #Créez un programme qui affiche la différence minimum absolue entre deux éléments d’un tableau constitué uniquement de nombres. Nombres négatifs acceptés.
+
+
+import sys 
+
+argument = sys.argv[1:]
+
+if len(argument) < 2:
+    print("Error")
+    sys.exit()
+
+nombre = []
+
+for arg in argument:
+    try:
+        num = int(arg)
+        nombre.append(num)
+    except ValueError:
+        print("Error")
+        sys.exit(1)
+
+difference = nombre[1] - nombre[0]
+if difference < 0:
+    difference = -difference
+for i in range(len(nombre)):
+    for j in range(i+1, len(nombre)):
+        diff = nombre[j] - nombre[i]
+        if diff < 0:
+            diff = -diff
+        if diff < difference:
+            difference = diff
+
+
+print(difference)
