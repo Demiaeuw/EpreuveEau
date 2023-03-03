9dbeedbc064156a3df3bc5b5065f1ad4ca753d54
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-03-03 16:38:12 +0100

    fin exo 10

--- eau10.py.py
+++ eau10.py.py
@@ -1,3 +1,17 @@
 #Index wanted
-#Créez un programme qui affiche le premier index d’un élément recherché dans un tableau. Le tableau est constitué de tous les arguments sauf le dernier.
-#L’élément recherché est le dernier argument. Afficher -1 si l’élément n’est pas trouvé.
+#Créez un programme qui affiche le premier index d’un élément recherché dans un tableau. Le tableau est cornstitué de tous les aguments sauf le dernier.
+#L’élément recherché est le dernier argument. Afficher -1 si l’élément n’est pas trouvé.*
+
+
+import sys
+
+argument_test = sys.argv[-1]
+argument = sys.argv[1:-1]
+
+
+try:
+    print(argument.index(argument_test))
+    sys.exit()
+except ValueError:
+    print("-1")
+    sys.exit()
