e6163a05de2e2ad429468b9cbd709b9c323df13a
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-03-03 17:10:27 +0100

    avance 14 a finir

--- eau14.py.py
+++ eau14.py.py
@@ -14,3 +14,27 @@ sorted_args = sorted(sys.argv[1:])
 
 for arg in sorted_args:
     print(arg, end=" ")
+
+
+
+"""
+import sys
+
+# Vérification du nombre d'arguments
+if len(sys.argv) < 2:
+    print("Error")
+    sys.exit()
+
+# Tri des arguments par ordre ASCII
+for i in range(1, len(sys.argv)):
+    min_idx = i
+    for j in range(i+1, len(sys.argv)):
+        if sys.argv[j] < sys.argv[min_idx]:
+            min_idx = j
+    if i != min_idx:
+        sys.argv[i], sys.argv[min_idx] = sys.argv[min_idx], sys.argv[i]
+
+# Affichage des arguments triés
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i], end=" ")
+"""
