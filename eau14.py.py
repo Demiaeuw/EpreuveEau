524c73fd337628382d32f40ff18d59399204855d
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-03-03 17:04:04 +0100

    avance exo 14

--- eau14.py.py
+++ eau14.py.py
@@ -1,2 +1,16 @@
 #Par ordre Ascii
 #Créez un programme qui trie les éléments donnés en argument par ordre ASCII.
+
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Error")
+    sys.exit()
+
+
+sorted_args = sorted(sys.argv[1:])
+
+
+for arg in sorted_args:
+    print(arg, end=" ")
