ff57b147720cc3860488d76f7da7bdac16b2c5aa
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-03-03 17:01:32 +0100

    fin exo 13

--- eau13.py.py
+++ eau13.py.py
@@ -7,3 +7,29 @@
 	# votre algorithme
 #	return (new_array)
 #}
+import sys 
+
+def selection_sort(array):
+    n = len(array)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if array[j] < array[min_idx]:
+                min_idx = j
+        
+        array[i], array[min_idx] = array[min_idx], array[i]
+    
+    return array
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+if all(is_int(arg) for arg in sys.argv[1:]):
+    result = selection_sort([int(arg) for arg in sys.argv[1:]])
+    print(' '.join(str(arg) for arg in result))
+else:
+    print("Error")
