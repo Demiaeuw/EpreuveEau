0e558d5b6d6fe397b581293c91d64700d3d253b3
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-03-08 09:50:09 +0100

    exo 14 fin

--- eau14.py.py
+++ eau14.py.py
@@ -17,24 +17,4 @@ for arg in sorted_args:
 
 
 
-"""
-import sys
 
-# Vérification du nombre d'arguments
-if len(sys.argv) < 2:
-    print("Error")
-    sys.exit()
-
-# Tri des arguments par ordre ASCII
-for i in range(1, len(sys.argv)):
-    min_idx = i
-    for j in range(i+1, len(sys.argv)):
-        if sys.argv[j] < sys.argv[min_idx]:
-            min_idx = j
-    if i != min_idx:
-        sys.argv[i], sys.argv[min_idx] = sys.argv[min_idx], sys.argv[i]
-
-# Affichage des arguments triés
-for i in range(1, len(sys.argv)):
-    print(sys.argv[i], end=" ")
-"""
